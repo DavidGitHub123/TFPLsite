5a76469f472ce20f9993910a9238c448a3f1b009
David Olsen <davidpaulolsen@gmail.com>
2025-02-14 19:34:32 -0700

    Happy Valentines Day

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../Assets/Img/msg-icon.png'
+import white_arrow from '../../Assets/Img/white-arrow.png'
+
+// export default function Contact () {
+    const Contact = () => {
+
+    // const [result, setResult] = React.useState("");
+
+    // const onSubmit = async (event) => {
+    //   event.preventDefault();
+    //   setResult("Sending....");
+    //   const formData = new FormData(event.target);
+  
+    //   formData.append("access_key", "db470873-fc1d-4453-81d1-c15e3e821227");
+  
+    //   const response = await fetch("https://api.web3forms.com/submit", {
+    //     method: "POST",
+    //     body: formData
+    //   });
+  
+    //   const data = await response.json();
+  
+    //   if (data.success) {
+    //     setResult("Form Submitted Successfully");
+    //     event.target.reset();
+    //   } else {
+    //     console.log("Error", data);
+    //     setResult(data.message);
+    //   }
+
+  return (
+    <div className='contact'>
+        <div className='contact-col'>
+            <h3>Send a Message <img src={msg_icon} alt='' /></h3>
+            <p>Reach out through the contact form to send a message.</p>
+        </div>
+        <div className='contact-col'>
+            {/* <form onSubmit={onSubmit}> */}
+            <form>
+                <label>Your Name</label>
+                <input type='text' name='name' placeholder='Enter Your Name' required/>
+                <label>Write your message here</label>
+                <textarea name='message' rows='6' placeholder='Enter Your Message' required></textarea>
+                <button className='btn dark-btn'>Submit Now <img src={white_arrow} alt='' /></button>
+                {/* <button type='submit' className='btn dark-btn'>Submit Now <img src={white_arrow} alt='' /></button> */}
+            </form>
+            {/* <span>{result}</span> */}
+        </div>
+    </div>
+  )
+}
+// }
+
+export default Contact
