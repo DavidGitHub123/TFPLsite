372262c225a9f06f39f1cd5015ded2975e8a1a0d
Reilynn Olsen <ReilynnOlsen@gmail.com>
2025-02-18 16:07:44 -0800

    add back button

--- src/Components/PrivacyPolicy/PrivacyPolicy.js
+++ src/Components/PrivacyPolicy/PrivacyPolicy.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router";
 
 export default function PrivacyPolicy() {
   return (
     <div>
       <p>some text</p>
+      <Link to="/">Back</Link>
     </div>
   );
 }
